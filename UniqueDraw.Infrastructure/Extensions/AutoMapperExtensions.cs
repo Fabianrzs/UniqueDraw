a233df118cba69c492da5c9ddee23a35da56643a
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-16 17:36:30 -0500

    Include extensions of dependecy

---
+++ UniqueDraw.Infrastructure/Extensions/AutoMapperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace UniqueDraw.Infrastructure.Extensions;
+
+public static class AutoMapperExtensions
+{
+    public static IServiceCollection AddMapperServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(AppAssembly.InfrastructureAssembly);
+        return services;
+    }
+}
---
+++ UniqueDraw.Infrastructure/Extensions/DomainServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using UniqueDraw.Domain.Attributes;
+
+namespace UniqueDraw.Infrastructure.Extensions;
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddDomainServices(this IServiceCollection services)
+    {
+        var _services = new List<Type>();
+
+        Assembly assembly = Assembly.Load(AppConstants.DomainProject);
+
+        _services.AddRange(assembly.GetTypes()
+               .Where(p => p.CustomAttributes.Any(x => x.AttributeType
+               == typeof(DomainServiceAttribute))));
+
+        _services.ForEach(serviceType => services.AddTransient(serviceType));
+
+        return services;
+    }
+}
