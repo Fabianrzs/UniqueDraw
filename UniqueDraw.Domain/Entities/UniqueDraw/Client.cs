2208a964bb008a0a1349d83cfdb60e9a6aa150c6
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-16 16:17:43 -0500

    Change entities domain

--- UniqueDraw.Domain/Entities/UniqueDraw/Client.cs
+++ UniqueDraw.Domain/Entities/UniqueDraw/Client.cs
@@ -1,12 +1,16 @@
-﻿using UniqueDraw.Domain.Entities.Base;
+﻿using UniqueDraw.Domain.Attributes;
+using UniqueDraw.Domain.Entities.Base;
 
 namespace UniqueDraw.Domain.Entities.UniqueDraw;
 
 public class Client : EntityBase
 {
+    [Encrypt]
     public string Name { get; set; } = string.Empty;
     public string ApiKey { get; set; } = string.Empty;
+    [Encrypt]
     public string? UserName { get; set; }
+
     public string? Password { get; set; }
     public ICollection<Raffle> Raffles { get; set; } = [];
     public ICollection<User> Users { get; set; } = [];
