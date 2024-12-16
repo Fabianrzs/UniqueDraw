d7ee7a502697885c3f2b8e4405d5b8a810c23c1a
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-12-16 16:18:32 -0500

    Add services helpers

---
+++ UniqueDraw.Domain/Ports/Helpers/IMappingService.cs
@@ -0,0 +1,9 @@
+﻿namespace UniqueDraw.Domain.Ports.Helpers;
+
+public interface IMappingService
+{
+    TDestination Map<TSource, TDestination>(TSource source);
+    TDestination Map<TDestination>(object source);
+    ICollection<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source);
+    ICollection<TDestination> Map<TDestination>(IEnumerable<object> source);
+}
